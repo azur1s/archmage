7b30351aabbf5380610faa98b90646fd2aa7bc50
azur <natapat.samutpong@gmail.com>
2022-09-14 20:52:42 +0700

    More arithmetic functions

--- src/Core.hs
+++ src/Core.hs
@@ -3,23 +3,48 @@ module Core where
 import Control.Monad.Trans (liftIO)
 import Types
 
-invalid :: String -> Int -> Except Value
-invalid fn amount = throwStr
+invalidArgAmount :: String -> Int -> Except Value
+invalidArgAmount fn amount = throwStr
     $ fn ++ ": Invalid number of arguments (expected " ++ show amount ++ ")"
+
+invalidArgTypes :: String -> [Value] -> Except Value
+invalidArgTypes fn args = throwStr
+    $ fn ++ ": Invalid argument types: " ++ show args
 
 core :: [(String, Fn)]
 core = [
+    -- Arithmetic functions
       ("+", \case
         [Int a, Int b]     -> return $ Int (a + b)
         [Float a, Float b] -> return $ Float (a + b)
-        [a, b]             -> throwStr $ "+: Invalid arguments " ++ show a ++ " and " ++ show b
-        _                  -> invalid "+" 2)
+        [a, b]             -> invalidArgTypes "+" [a, b]
+        _                  -> invalidArgAmount "+" 2)
+    , ("-", \case
+        [Int a, Int b]     -> return $ Int (a - b)
+        [Float a, Float b] -> return $ Float (a - b)
+        [a, b]             -> invalidArgTypes "-" [a, b]
+        _                  -> invalidArgAmount "-" 2)
+    , ("*", \case
+        [Int a, Int b]     -> return $ Int (a * b)
+        [Float a, Float b] -> return $ Float (a * b)
+        [a, b]             -> invalidArgTypes "*" [a, b]
+        _                  -> invalidArgAmount "*" 2)
+    , ("/", \case
+        [Int a, Int b]     -> return $ Int (a `div` b)
+        [Float a, Float b] -> return $ Float (a / b)
+        [a, b]             -> invalidArgTypes "/" [a, b]
+        _                  -> invalidArgAmount "/" 2)
+    , ("%", \case
+        [Int a, Int b]     -> return $ Int (a `mod` b)
+        [a, b]             -> invalidArgTypes "%" [a, b]
+        _                  -> invalidArgAmount "%" 2)
+    -- IO Functions
     , ("print", \case
         [a] -> liftIO $ putStr (fmtValue a) >> return nil
-        _   -> invalid "print" 1)
+        _   -> invalidArgAmount "print" 1)
     , ("println", \case
         [a] -> liftIO $ putStr (fmtValueLn a) >> return nil
-        _   -> invalid "println" 1)
+        _   -> invalidArgAmount "println" 1)
     , ("str", return . Str . concatMap fmtValue)
     ]
 
